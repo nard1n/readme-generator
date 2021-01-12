6dac6849f1c282a7d046e6c1044556bdb1adc2e8
Nardin Lachowski <nardin@live.com>
2021-01-11 20:12:31 -0500

    adding project files to repo, including the README.md generated using the application itself. Pretty cool, huh?

---
+++ README.md
@@ -0,0 +1,41 @@
+## README.md Generator
+
+    ## Description
+    [![License](https://img.shields.io/badge/License-BSD-2-Clause-yellow.svg)]
+    This is a simple Node.JS CLI app that helps a user generate a README.md file for their project repositories. It prompts the user to enter essential information about their repository, such as: title, description, installation instructions, usage information, contribution guidelines, and test instructions. It will display all of the information in a professional manner in separated sections along with a table of contents containing links to corresponding sections. It also includes a Questions section with a link to Github profile and an email address based on user input
+
+    ## Table of Contents
+
+    * [Installation](#Installation)
+    * [Usage](#Usage)
+    * [Contributing](#Contributing)
+    * [Tests](#Tests)
+    * [License](#License)
+    * [Questions](#Questions)
+
+    ## Installation
+    To install, simply clone or download the repository
+
+    ## Usage
+    To use this application and create your own README.md file: 
+    1. Make sure Node.JS is installed on your machine 
+    2. Navigate to your terminal 
+    3. select the directory where this application is saved by using the cd command (e.g. cd Desktop/readme-generator) 
+    4. run the command "npm install" 
+    5. Run the command "node index.js" 
+    6. You will see a prompt of questions, you will one at a time. Remember to answer them carefully as this will generate your file once you complete the questions. 
+
+    ## Contributing
+    We love seeing community contribution to any opensource project! If you would like to contribute, please do.
+
+    ## Tests
+    There aren't any tests to run ahead of using this application. However, always remember to test often when you make your own changes.
+
+    ## License
+    This project is released under [BSD-2-Clause|https://opensource.org/licenses/BSD-2-Clause]
+
+    ## Questions
+    For more about my work, check out my Github profile: https://github.com/nard1n
+
+    If you have any questions and would like to chat, please feel free to send me 
+    an email directly to nardin.lachowski@gmail.com
---
+++ index.js
@@ -0,0 +1,135 @@
+// packages needed for application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const util = require('util');
+
+// creating promise function (using promisify from util module)
+const writeFileAsync = util.promisify(fs.writeFile);
+
+// array of questions for user input
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Enter description of project',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Enter installation instructions',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Enter usage information',
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'Enter contribution guidelines',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Any tests a developer should complete?',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Choose a license for your application',
+            choices: [
+                "MIT",
+                "Apache-2.0",
+                "BSD-2-Clause",
+                "Simple-2.0",
+                "Unlicense"
+            ],
+        },
+        {
+            type: 'list',
+            name: 'license badge',
+            message: 'Select the badge for selected license',
+            choices: [
+                "MIT",
+                "Apache 2.0",
+                "BSD 2-Clause",
+                "Simple",
+                "Unlicense"
+            ],
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your Github username?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email?',
+        }
+    ]);
+};
+
+// content of README file
+const generateReadMe = (answers) =>
+    `# ${answers.title}
+
+    ## Description
+    [![License](https://img.shields.io/badge/License-${answers.license}-yellow.svg)]
+    ${answers.description}
+
+    ## Table of Contents
+
+    * [Installation](#Installation)
+    * [Usage](#Usage)
+    * [Contributing](#Contributing)
+    * [Tests](#Tests)
+    * [License](#License)
+    * [Questions](#Questions)
+
+    ## Installation
+    ${answers.installation}
+
+    ## Usage
+    ${answers.usage}
+
+    ## Contributing
+    ${answers.contributing}
+
+    ## Tests
+    ${answers.tests}
+
+    ## License
+    This project is released under [${answers.license}|https://opensource.org/licenses/${answers.license}]
+
+    ## Questions
+    For more about my work, check out my Github profile: https://github.com/${answers.github}
+
+    If you have any questions and would like to chat, please feel free to send me 
+    an email directly to ${answers.email}`;
+
+// function to initialize app and write README file after prompts are answered
+////function writeToFile(fileName, data) {}
+const init = async () => {
+    //console.log('hi'); //test init
+    try {
+        const answers = await questions();
+
+        const readme = generateReadMe(answers);
+
+        await writeFileAsync('README.md', readme);
+
+        console.log('Successfully wrote README.md');
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+// Function call to initialize app
+init();
